e1a6fe9c932a75565ac2758ac092552734838e8c
keijidosha <ko322jp+github@gmail.com>
2018-01-02 17:51:11 +0900

    DefaultRunner depends a application.

--- retryer/Runner.kt
+++ retryer/Runner.kt
@@ -3,9 +3,3 @@ package retryer
 interface Runner<T> {
     fun run( f: () -> T ): T
 }
-
-object DefaultRunner: Runner<String> {
-    override fun run( f: () -> String ): String {
-        return f()
-    }
-}
